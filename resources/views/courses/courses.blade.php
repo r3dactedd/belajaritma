508e34f049446f17b677b9e5c169370a0614b8ab
Pixelshiftx <vitosuyata@gmail.com>
2023-07-31 12:43:10 +0700

    Course layout fix

--- resources/views/courses/courses.blade.php
+++ resources/views/courses/courses.blade.php
@@ -13,9 +13,9 @@
 
 <body class="bg-gray-200 pb-12">
     @section('title', 'Homepage')
-    @extends('layout')
+    @extends('layout.layout')
     @section('header')
-        @include('header')
+        @include('layout.header')
     @endsection
     @section('content')
 
@@ -176,8 +176,9 @@
         </p>
     @endsection
     @section('footer')
-        @include('footer')
+        @include('layout.footer')
     @endsection
+
 </body>
 
 
