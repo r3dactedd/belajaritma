e36c273e47b74123a612df700f629afa3a25305f
Pixelshiftx <vitosuyata@gmail.com>
2023-12-04 22:54:34 +0700

    transaction addition + some UI edits

--- resources/views/administrator/admin_courses/admin_course_edit.blade.php
+++ resources/views/administrator/admin_courses/admin_course_edit.blade.php
@@ -223,7 +223,7 @@
                                                 d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                         </svg>
                                     </a>
-                                    <div class="mr-2 w-4 transform hover:scale-110 hover:text-purple-500"
+                                    <div class="mr-2 w-4 transform hover:scale-110 hover:text-red-500"
                                         data-modal-target="popup-delete" data-modal-toggle="popup-delete">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke="currentColor">
@@ -251,7 +251,7 @@
                                                 d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                         </svg>
                                     </a>
-                                    <div class="mr-2 w-4 transform hover:scale-110 hover:text-purple-500"
+                                    <div class="mr-2 w-4 transform hover:scale-110 hover:text-red-500"
                                         data-modal-target="popup-delete" data-modal-toggle="popup-delete">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke="currentColor">
