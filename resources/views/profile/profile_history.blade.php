9f992f887c9e380af9f89fb82e08ef03a73d2587
Pixelshiftx <vitosuyata@gmail.com>
2023-12-05 18:23:44 +0700

    Add transact history

---
+++ resources/views/profile/profile_history.blade.php
@@ -0,0 +1,75 @@
+<div class="container mx-auto my-4 pb-4">
+    <table class="mx-auto w-full text-xs text-white">
+        <colgroup>
+            <col>
+            <col>
+            <col>
+            <col>
+            <col>
+        </colgroup>
+        <thead class="bg-gray-200 leading-normal text-gray-600">
+            <tr class="text-md bg-gray-200 leading-normal text-gray-600 md:text-lg">
+                <th class="px-4 py-2 text-left">Nama Sertifikasi</th>
+                <th class="px-4 py-2 text-left">Kode Pembayaran</th>
+                <th class="px-4 py-2 text-left">Tanggal Upload Bukti</th>
+                <th class="px-4 py-2 text-left">Status Pembayaran</th>
+                <th class="px-2 py-3 text-center">Foto Bukti</th>
+
+            </tr>
+        </thead>
+        <tbody class="text-md font-light text-gray-600 md:text-lg">
+
+            <tr class="border-b border-opacity-20 bg-white font-medium leading-normal text-gray-600">
+                <td class="px-4 py-2">
+                    <p class="max-h-20 overflow-scroll">Cone Things</p>
+                </td>
+                <td class="px-4 py-2">
+                    <p class="max-h-20 overflow-scroll">AB13991548</p>
+                </td>
+                <td class="px-4 py-2">
+                    <p class="max-h-20 overflow-scroll">21 December 2069</p>
+                </td>
+                <td class="px-4 py-2">
+                    <p class="max-h-20 overflow-scroll text-green-500">Berhasil</p>
+                </td>
+                <td class="px-6 py-3 text-center">
+                    <div class="item-center flex justify-center">
+                        {{-- Image here --}}
+                        <a href="#" class="mr-2 w-4 transform hover:scale-110 hover:text-green-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512">
+                                <path
+                                    d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
+                            </svg>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+            <tr class="border-b border-opacity-20 bg-white font-medium leading-normal text-gray-600">
+                <td class="px-4 py-2">
+                    <p class="max-h-20 overflow-scroll">Pog Things</p>
+                </td>
+                <td class="px-4 py-2">
+                    <p class="max-h-20 overflow-scroll">3155agfh2</p>
+                </td>
+                <td class="px-4 py-2">
+                    <p class="max-h-20 overflow-scroll">21 December 2077</p>
+                </td>
+                <td class="px-4 py-2">
+                    <p class="max-h-20 overflow-scroll text-red-500">Gagal</p>
+                </td>
+                <td class="px-6 py-3 text-center">
+                    <div class="item-center flex justify-center">
+                        {{-- Image here --}}
+                        <a href="#" class="mr-2 w-4 transform hover:scale-110 hover:text-green-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512">
+                                <path
+                                    d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
+                            </svg>
+                        </a>
+                    </div>
+                </td>
+
+            </tr>
+        </tbody>
+    </table>
+</div>
