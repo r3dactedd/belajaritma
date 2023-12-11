ff54a4fb33c82f0a6242eafd764a647f567be58a
Azzel04 <azzelreyhanth@gmail.com>
2023-12-12 00:11:22 +0700

    branch:-Azzel: Adjust for Reply Comment feature

--- resources/views/forum/forum_thread.blade.php
+++ resources/views/forum/forum_thread.blade.php
@@ -287,7 +287,7 @@
                             </p>
                             <form id="myForm2" method="post" enctype="multipart/form-data">
                                 @csrf
-                                <textarea id="forum_message" class="h-24" placeholder="Input Pertanyaan Anda disini."></textarea>
+                                <textarea id="forum_message2" class="h-24" placeholder="Input Pertanyaan Anda disini."></textarea>
                                 <input type="hidden" id="replyId" name="reply_id" value="{{ $data->id }}">
                                 <input type="hidden" id="courseId" name="course_id" value="{{ $data->course_id }}">
 
@@ -370,6 +370,51 @@
             content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 
         })
+
+        tinymce.init({
+            selector: '#forum_message2',
+            height: 200,
+            menubar: false,
+            plugins: ' code codesample image',
+            toolbar: ' wordcount | link image |code |bold italic underline| codesample ',
+            // Image below, for further consideration
+            file_picker_types: 'image',
+            /* enable automatic uploads of images represented by blob or data URIs*/
+            automatic_uploads: true,
+            file_picker_callback: (cb, value, meta) => {
+                const input = document.createElement('forum_message2');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');
+
+                input.addEventListener('change', (e) => {
+                    const file = e.target.files[0];
+
+                    const reader = new FileReader();
+                    reader.addEventListener('load', () => {
+                        /*
+                          Note: Now we need to register the blob in TinyMCEs image blob
+                          registry. In the next release this part hopefully won't be
+                          necessary, as we are looking to handle it internally.
+                        */
+                        const id = 'blobid' + (new Date()).getTime();
+                        const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                        const base64 = reader.result.split(',')[1];
+                        const blobInfo = blobCache.create(id, file, base64);
+                        blobCache.add(blobInfo);
+
+                        /* call the callback and populate the Title field with the file name */
+                        cb(blobInfo.blobUri(), {
+                            title: file.name
+                        });
+                    });
+                    reader.readAsDataURL(file);
+                });
+
+                input.click();
+            },
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+
+        })
         document.addEventListener("DOMContentLoaded", function(event) {
 
             const accordionHeader = document.querySelectorAll(".accordion-header");
@@ -406,17 +451,18 @@
             event.preventDefault(); // Prevent the default form submission behavior
             submitForm();
         });
+
         document.getElementById('myForm2').addEventListener('submit', function(event) {
-            console.log("Hi, ini code getElementnya jalan!")
+            console.log("Hi, ini code getElementnya jalan Versi Reply Comment!")
             event.preventDefault(); // Prevent the default form submission behavior
-            submitForm();
+            submitForm2();
         });
 
         function submitForm() {
             var editorContent = tinymce.get('forum_message').getContent();
             console.log("ini isian editorContent", editorContent)
-            var hasImages = editorContent.includes('<img');
-            var fileInput = document.getElementById('forum_attachment');
+            // var hasImages = editorContent.includes('<img');
+            // var fileInput = document.getElementById('forum_attachment');
             var courseId = document.getElementById('courseId').value;
             console.log("ini isian courseId", courseId)
             var replyId = document.getElementById('replyId').value;
@@ -464,8 +510,60 @@
                     // Handle errors, including non-JSON responses
                     console.error('Error:', error);
                 });
+        }
+
+        function submitForm2() {
+            var editorContent = tinymce.get('forum_message2').getContent();
+            console.log("ini isian editorContent", editorContent)
+            // var hasImages = editorContent.includes('<img');
+            // var fileInput = document.getElementById('forum_attachment');
+            var courseId = document.getElementById('courseId').value;
+            console.log("ini isian courseId", courseId)
+            var replyId = document.getElementById('replyId').value;
+            console.log("ini isian replyId", replyId)
 
 
+            console.log('CSRF Token:', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
+            var formData = new FormData();
+            formData.append('course_id', courseId);
+            formData.append('forum_message', editorContent);
+            formData.append('reply_id', replyId);
+
+
+            // if (hasImages) {
+            //     var file = fileInput.files[0];
+            //     formData.append('forum_attachment', file);
+            // }
+
+            fetch('/forum/course/' + courseId + '/thread/' + replyId, {
+                    method: 'POST',
+                    body: formData,
+                    headers: {
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                    },
+                })
+                .then(response => {
+                    if (response.ok) {
+                        try {
+                            return response.json();
+                        } catch (error) {
+                            console.error('Error parsing JSON:', error);
+                            return null; // or handle the error in an appropriate way
+                        }
+                    } else {
+                        console.error('Request failed with status:', response.status);
+                        return null; // or handle the error in an appropriate way
+                    }
+                })
+                .then(data => {
+                    console.log('Success:', data);
+                    // Redirect to a new page using JavaScript
+                    window.location.href = '/forum/course/' + courseId + '/thread/' + replyId;
+                })
+                .catch(error => {
+                    // Handle errors, including non-JSON responses
+                    console.error('Error:', error);
+                });
         }
     </script>
 
