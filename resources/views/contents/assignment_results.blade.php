7ee459c2c62c8f21ec624fde4c57fbb30427a386
Azzel04 <azzelreyhanth@gmail.com>
2023-12-26 22:09:43 +0700

    commit branch-Azzel: Indexing for Sessions

--- resources/views/contents/assignment_results.blade.php
+++ resources/views/contents/assignment_results.blade.php
@@ -27,7 +27,7 @@
                                 <path
                                     d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z" />
                             </svg>
-                            <span class="mb-1 ml-2">Sesi x : Nama Materi </span>
+                            <span class="mb-1 ml-2">Sesi {{ $currentMaterialIndex + 1 }} : Nama Materi </span>
                         </a>
                 </div>
             </div>
