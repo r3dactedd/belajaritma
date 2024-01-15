9674e3bcb195356e80b93e032998783aafcfb3d3
Azzel04 <azzelreyhanth@gmail.com>
2024-01-15 22:30:17 +0700

    branch-Azzel-Final: Fix Title when show Result in course

--- resources/views/contents/assignment_test_results.blade.php
+++ resources/views/contents/assignment_test_results.blade.php
@@ -27,7 +27,7 @@
                                 <path
                                     d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z" />
                             </svg>
-                            <span class="mb-1 ml-2">Sesi {{ $sidebars->order }} : {{ $sidebars->title }}</span>
+                            <span class="mb-1 ml-2">Sesi {{ $currentMaterialIndex + 1 }} : {{ $material->title }}</span>
                         </a>
                 </div>
             </div>
@@ -149,7 +149,6 @@
                                     <h2
                                         class="relative w-auto py-4 mx-6 mb-2 font-semibold tracking-normal text-gray-800 text-md lg:text-md">
                                         {{ $remainingTime }} Menit Hingga Uji Coba Ulang Berikutnya</h2>
-                             
                                 @endif
 
                             </div>
