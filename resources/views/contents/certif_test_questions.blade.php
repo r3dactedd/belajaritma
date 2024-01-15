5a05216dfcc0f9a9a5aa7b2c0c4b8b87b22be34c
Azzel04 <azzelreyhanth@gmail.com>
2024-01-16 00:08:03 +0700

    branch-Azzel-Final: Keluar dari (insert Type) akan menghanguskan attempt

--- resources/views/contents/certif_test_questions.blade.php
+++ resources/views/contents/certif_test_questions.blade.php
@@ -61,9 +61,56 @@
                     <div class="w-full my-2 flex justify-center text-center rounded-md shadow h-fit md:w-9/12">
 
 
-                        <button id="exit-asg"
-                            class="w-full my-2 mr-2 justify-center text-center items-center rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:focus:ring-red-800 shadow h-fit md:w-9/12">Keluar
-                            dari Tes Sertifikasi</button>
+                        <a id="exit-final" data-modal-target="popup-exit-cert" data-modal-toggle="popup-exit-cert"
+                            class="w-full my-2 mr-2 justify-center text-center items-center rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:focus:ring-red-800 shadow h-fit md:w-9/12">
+                            Keluar dari Tes Sertifikasi
+                        </a>
+                        <div id="popup-exit-cert" tabindex="-1"
+                            class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden p-4 md:inset-0">
+                            <div class="relative max-h-full w-full max-w-md">
+                                <div class="relative rounded-lg bg-white shadow dark:bg-gray-700">
+                                    <button type="button"
+                                        class="absolute right-2.5 top-3 ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+                                        data-modal-hide="popup-exit-cert">
+                                        <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                        </svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                    <div class="p-6 text-center">
+                                        <svg class="mx-auto mb-4 h-12 w-12 text-gray-400 dark:text-gray-200"
+                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                            viewBox="0 0 20 20">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                        </svg>
+                                        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                                            Apakah kamu yakin ingin membatalkan pengerjaan tes sertifikasi?
+                                        </h3>
+                                        <strong>
+                                            Attempt pengerjaan tes sertifikasi ini akan hangus
+                                        </strong>
+                                        <div class="flex justify-center text-center">
+                                            <form method="GET" action="/certification/{{ $certif_id }}/exitCertTest">
+                                                @csrf
+                                                <button type="submit"
+                                                    class="mr-2 items-center rounded-lg bg-red-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-red-300 dark:focus:ring-red-800">
+                                                    Ya
+                                                </button>
+                                            </form>
+                                            <button type="button"
+                                                class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-500 hover:bg-indigo-400 hover:text-white focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200"
+                                                data-modal-hide="popup-exit-final">
+                                                Tidak
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
@@ -183,7 +230,7 @@
             document.addEventListener('DOMContentLoaded', function() {
                 const nextButton = document.getElementById('tombol-selanjutnya');
                 const previousButton = document.getElementById('tombol-sebelumnya');
-                const exitAsg = document.getElementById('exit-asg');
+                // const exitAsg = document.getElementById('exit-asg');
                 const submitButton = document.getElementById('submit-button');
                 // localStorage.removeItem('userAnswers');
                 let userAnswers = JSON.parse(localStorage.getItem('userAnswers')) || [];
@@ -343,24 +390,21 @@
                 }
 
 
-                if (exitAsg) {
-                    exitAsg.addEventListener('click', function() {
-                        const isConfirmed = window.confirm(
-                            'Apakah kamu yakin ingin membatalkan pengerjaan assignment? semua progress jawaban kamu akan hilang jika keluar'
-                        );
+                // if (exitAsg) {
+                //     exitAsg.addEventListener('click', function() {
+                //         const isConfirmed = window.confirm(
+                //             'Apakah kamu yakin ingin membatalkan pengerjaan assignment? semua progress jawaban kamu akan hilang jika keluar'
+                //         );
 
-                        if (isConfirmed) {
-                            const certifId = document.getElementById('id').value;
-                            clearSelectedAnswers();
-                            localStorage.removeItem('timerCert');
-                            @php
-                                // session()->forget('shuffledQuestionIds');
-                                // session()->forget('reshuffled');
-                            @endphp
-                            window.location.href = '/certifications/aboutTest/' + certifId
-                        }
-                    });
-                }
+                //         if (isConfirmed) {
+                //             const certifId = document.getElementById('id').value;
+                //             clearSelectedAnswers();
+                //             localStorage.removeItem('timerCert');
+
+                //             window.location.href = '/certifications/aboutTest/' + certifId
+                //         }
+                //     });
+                // }
 
 
 
